13f23b7d82d567d8c1b06ee52d8cac0bb5f695aa
desolatttte1212 <somovandrej10@gmail.com>
2025-11-30 13:21:46 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cryptocore",
-    version="0.1.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         'pycryptodome>=3.20.0',
